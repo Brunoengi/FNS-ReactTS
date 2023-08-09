cd86dfba5a6651414ef3be45e283ab3c74b1156f
Bruno <bruno.if@hotmail.com>
2023-08-09 00:21:39 -0300

    get partial inputs

--- src/types/ISelect.tsx
+++ src/types/ISelect.tsx
@@ -1,5 +1,8 @@
 export default interface ISelect {
     dataset: Array<number | string>
+    context: React.ReactNode | string
+    labelSelect?: string
+    endText?: string | undefined
 }
 
 export interface IdataValue {
